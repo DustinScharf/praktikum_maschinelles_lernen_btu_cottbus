0aea50a4a18af96216b352dc8b18c3db79cd2dd4
Dustin Scharf <d.scharf.2002@gmail.com>
2022-11-23 15:26:50 +0100

    Did some things

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -7,21 +7,22 @@ class NeuralNetwork:
         self.hidden_nodes = hidden_nodes
         self.output_nodes = output_nodes
 
-        self.weights_input_to_hidden = np.random.normal(0.0, pow(self.hidden_nodes, -0.5),
-                                                        (self.hidden_nodes, self.input_nodes))
-        self.weights_hidden_to_output = np.random.normal(0.0, pow(self.output_nodes, -0.5),
-                                                         (self.output_nodes, self.hidden_nodes))
+        np.random.seed(12345)
+        self.weights_input_to_hidden = np.random.normal(0.0, pow(self.hidden_nodes, -0.5), self.hidden_nodes)
+        self.weights_hidden_to_output = np.random.normal(0.0, pow(self.output_nodes, -0.5), self.hidden_nodes)
 
-        self.bias_hidden = np.array([[1]] * self.hidden_nodes)
+        # self.bias_hidden = np.array([[1]] * self.hidden_nodes)
+        self.bias_hidden = np.ones(self.hidden_nodes, dtype=np.float64)
         # self.bias_output = np.array([[1]] * self.output_nodes)  # not a task, but experimental
 
     def activation_function(self, x):  # die sigmund funktion eigentlich
         return 1 / (1 + np.exp(-x))
 
     def predict(self, input_list):
-        inputs = np.array(input_list, ndmin=2).T
+        inputs = np.array(input_list)
+        # inputs = np.array(input_list, ndmin=2)
 
-        hidden_inputs = np.dot(self.weights_input_to_hidden, inputs)
+        hidden_inputs = np.multiply(self.weights_input_to_hidden, inputs)
         hidden_inputs = np.add(hidden_inputs, self.bias_hidden)
         hidden_outputs = self.activation_function(hidden_inputs)
 
@@ -36,13 +37,10 @@ class NeuralNetwork:
 
         beta = 0.9
 
-        theta_new = np.stack(
-            (self.weights_hidden_to_output.T, self.bias_hidden, self.weights_input_to_hidden),
-            axis=1
-        ).flatten()
-
-        theta_yet = theta_new
-        theta_old = theta_yet  # todo fragen: oder 0 setzen?
+        theta_new = []
+        theta_yet = self.weights_input_to_hidden, self.bias_hidden, self.weights_hidden_to_output
+        theta_old = 0, 0, 0
+        delta_theta_old = 0, 0, 0
 
         error_yet = np.inf  # todo maybe error
 
@@ -51,15 +49,20 @@ class NeuralNetwork:
         for k, data in enumerate(train_data):
             result = self.predict(data)
 
-            theta_new = theta_yet - alpha * ABLEITUNG + beta * DELTA * theta_old  # todo
+            derivation = (result - train_labels) * theta_yet[2] * data * \
+                         ((theta_yet[0] * data + theta_yet[1]) * (1 - (theta_yet[0] * data + theta_yet[1])))
+            theta_new[0] = theta_yet[0] - alpha * derivation + beta * delta_theta_old[0]
+
+            # todo update theta_new[1] and theta_new[2]
 
             error_new = self.E(train_labels, result)
 
-            # todo update weights according to theta_new
+            # todo update weights according to theta_new (or is it already referenced?)
 
             alpha = alpha if np.less(error_new, error_yet) else alpha / 2
 
-            theta_old = theta_yet
+            delta_theta_old = -alpha * derivation + beta * delta_theta_old
+            # theta_old = theta_yet
             theta_yet = theta_new
 
             error_yet = error_new
--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from NeuralNetwork import NeuralNetwork
 
 if __name__ == '__main__':
     neural_network = NeuralNetwork(1, 3, 1)
-    # print(neural_network.predict([1]))
-    print(neural_network.learn([1], 1))
+    print(neural_network.predict([1]))
+    print(neural_network.learn([1], [1]))
